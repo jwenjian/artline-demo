da914f677ab90e875cb474ecd3006121711fc375
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-12-27 20:52:52 +0800

    docker file works
    docker build .
    docker run -d -p 5000:5000 <image id>

--- app.py
+++ app.py
@@ -1,44 +1,13 @@
-import datetime
-import os
-from uuid import uuid1
 import base64
-
-from flask import Flask, request, render_template
 import urllib
+from uuid import uuid1
 
 import PIL.Image
 import torchvision.transforms as T
 from fastai.vision import *
 from fastai.vision import load_learner
-
-
-class FeatureLoss(nn.Module):
-    def __init__(self, m_feat, layer_ids, layer_wgts):
-        super().__init__()
-        self.m_feat = m_feat
-        self.loss_features = [self.m_feat[i] for i in layer_ids]
-        self.hooks = hook_outputs(self.loss_features, detach=False)
-        self.wgts = layer_wgts
-        self.metric_names = ['pixel', ] + [f'feat_{i}' for i in range(len(layer_ids))
-                                           ] + [f'gram_{i}' for i in range(len(layer_ids))]
-
-    def make_features(self, x, clone=False):
-        self.m_feat(x)
-        return [(o.clone() if clone else o) for o in self.hooks.stored]
-
-    def forward(self, input, target):
-        out_feat = self.make_features(target, clone=True)
-        in_feat = self.make_features(input)
-        self.feat_losses = [base_loss(input, target)]
-        self.feat_losses += [base_loss(f_in, f_out) * w
-                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
-        self.feat_losses += [base_loss(gram_matrix(f_in), gram_matrix(f_out)) * w ** 2 * 5e3
-                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
-        self.metrics = dict(zip(self.metric_names, self.feat_losses))
-        return sum(self.feat_losses)
-
-    def __del__(self): self.hooks.remove()
-
+from flask import Flask, request, render_template
+from core import FeatureLoss
 
 def demo_show(the_img: Image, ax: plt.Axes = None, figsize: tuple = (3, 3), title: Optional[str] = None,
               hide_axis: bool = True,
@@ -110,4 +79,4 @@ def result_view():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
